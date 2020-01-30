d5a595d4413ae0b019e4e40c1d90b004425fc6fa
Hannes Preishuber <hannesp@ppedv.de>
2020-01-30 16:10:03 +0100

    MVVM Model in Blazor reuse

---
+++ Training2/Data/SharedModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace Training2.Data
+{
+    public class SharedModel
+    {
+       
+
+        private int _Zahl;
+
+        public int Zahl
+        {
+            get { return _Zahl; }
+            set { _Zahl = value;
+                PropertyChanged();
+            }
+        }
+        //analog zu MVVM aus WPF und UPW gedacht
+        public event EventHandler PropertyChangedEvent;
+        public async void PropertyChanged([CallerMemberName] string feldname=null)
+        {
+            PropertyChangedEvent?.Invoke(this, EventArgs.Empty);
+
+
+        }
+
+    }
+
+}
--- Training2/Startup.cs
+++ Training2/Startup.cs
@@ -29,6 +29,7 @@ namespace Training2
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddSingleton<SharedModel>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
