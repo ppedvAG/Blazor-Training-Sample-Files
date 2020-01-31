082e757e14c8b156b983f6ce648857613c9e37e7
Hannes Preishuber <hannesp@ppedv.de>
2020-01-31 10:22:22 +0100

    Formular Validierung

--- Training2/Data/Cars.cs
+++ Training2/Data/Cars.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,7 @@ namespace Training2.Data
 {
     public class Cars
     {
-       
+     [MaxLength(6,ErrorMessage ="max 6 zeichen")]  
         public string Hersteller { get; set; }
        public virtual Type[] Type { get; set; }
     }
