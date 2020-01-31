93fa25283b014572761362736c3b80fb401f6897
Hannes Preishuber <hannesp@ppedv.de>
2020-01-31 09:13:31 +0100

    ViewModel Kaskadierendes Dropdown

---
+++ Training2/Data/Cars.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Training2.Data
+{
+    public class Cars
+    {
+       
+        public string Hersteller { get; set; }
+       public virtual Type[] Type { get; set; }
+    }
+    public class Type
+    {
+        public Type(string _Name)
+        {
+            Name = _Name;
+        }
+        public string Name { get; set; }
+
+    }
+}
---
+++ Training2/Data/CarsModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Training2.Data
+{
+    public class CarsModel
+    {
+        public bool Off { get; set; }
+        public string Auto { get; set; }
+        public string Typ { get; set; }
+
+        public List<Cars> AutoListe { get; set; }
+        public CarsModel()
+        {
+            AutoListe = new List<Cars>();
+            AutoListe.Add(new Cars()
+            {
+                Hersteller = "BMW",
+                Type = new Type[]{ new Type("X1"), new Type("X2"), new Type("M3"), new Type("I3") }
+            });
+            AutoListe.Add(new Cars()
+            {
+                Hersteller = "VW",
+                Type = new Type[] { new Type("Golf"), new Type("Polo"), new Type("Beatle"), new Type("Touran") }
+            });
+            AutoListe.Add(new Cars()
+            {
+                Hersteller = "Chevrolet",
+                Type = new Type[] { new Type("Camaro"), new Type("Captiva"), new Type("Cruze"), new Type("Volt") }
+            });
+
+
+        }
+    }
+}
